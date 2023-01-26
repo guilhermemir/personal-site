bf1cb2fca46d197a6d335a2003f3717645d3087b
Guilherme Mir <guilherme.mir@gmail.com>
2023-01-26 16:08:14 -0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,19 @@ npm run build
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+echo > ./dist/.nojekyll
 
 # if you are deploying to a custom domain
-echo 'www.guilhermemir.com' > CNAME
+echo 'www.guilhermemir.com' > ./dist/CNAME
 
-git init
-git checkout -B main
-git add -A
+# git init
+# git checkout -B main
+git add dist
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:guilhermemir/guilhermemir.github.io.git main
+# git push -f git@github.com:guilhermemir/guilhermemir.github.io.git main
+git subtree push --prefix dist origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
