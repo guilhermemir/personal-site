7936c32e50be562b7363fa06f2eb2880410bf26d
Guilherme Mir <guilherme.mir@gmail.com>
2023-01-29 20:29:58 -0300

    update page + add images and icons

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css'
+
+const block_to_insert = document.createElement( 'span' )
+block_to_insert.innerHTML = new Date().getFullYear()
+ 
+const container_block = document.getElementById( 'current-year' )
+container_block.appendChild( block_to_insert )
